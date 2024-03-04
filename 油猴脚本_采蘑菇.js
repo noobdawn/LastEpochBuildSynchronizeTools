870c4d2dc6d0fa19c07a6c01b796c26bedbaacb9
Noobdawn <694109410@qq.com>
2024-03-04 14:11:29 +0800

    fit the url

--- 油猴脚本_采蘑菇.js
+++ 油猴脚本_采蘑菇.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://lastepoch.caimogu.cc/planner.html?id=*
+// @match        https://lastepoch.caimogu.cc/planner?id=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=caimogu.cc
 // @grant        none
 // ==/UserScript==
